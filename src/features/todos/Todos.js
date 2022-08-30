ce7f0bb1a8850c215c761d34feb81e7769306fe9
Ahmed Al-Qossab <alqossaba@gmail.com>
2022-08-29 23:54:18 -0400

    rough delete

--- src/features/todos/Todos.js
+++ src/features/todos/Todos.js
@@ -1,10 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from './todosSlice';
+
 const Todos = ({ todo }) => {
     const { text } = todo;
 
+    const dispatch = useDispatch();
+
+    const handleDelete = () => {
+        dispatch(deleteTodo({ id}));
+    };
+
     return (
-        <li>{text}</li>
+        <>
+            <li>{text} <button onClick={handleDelete}>Delete</button></li>
+        </>
     );
 };
-
 
 export default Todos;
